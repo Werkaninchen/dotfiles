56472af059cd5aa90c24eff807b0ff57c7f67e99
Werkaninchen <jeroen.trzaska@gmail.com>
2019-02-03 12:07:18 +0000

    fixed set script

--- setFiles.sh
+++ setFiles.sh
@@ -1,5 +1,7 @@
-!/bin/bash
-cp /vimrc ~/vimrc
-cp /terminator ~/.config/terminator
-cp /i3 ~/.config/i3
-cp /ZSH/zshrc ~/.zshrc
+#!/bin/bash
+cp vimrc $HOME/vimrc
+cp terminator $HOME/.config/terminator
+cp i3 $HOME/.config/i3
+cp ZSH/zshrc $HOME/.zshrc
+cp tmux.conf $HOME/.tmux.conf
+
