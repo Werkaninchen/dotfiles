b6f954bc40fae86835a9c05d52e1860977e119ce
Werkaninchen <jeroen.trzaska@gmail.com>
2019-02-03 12:04:47 +0000

    added config files

--- getFiles.sh
+++ getFiles.sh
@@ -1,5 +1,12 @@
-!/bin/bash
-cp ~/.config/terminator /terminator
-cp ~/.config/i3 /i3
-cp ~/.vimrc /vimrc
-cp ~/.zshrc /ZSH/zshrc
+#!/bin/bash
+
+cp -r $HOME/.config/terminator terminator
+if [ ! -f /.config/i3 ]; then
+	
+      cp -r $HOME/.config/i3 i3
+   
+	fi
+cp $HOME/.vimrc vimrc
+cp $HOME/.zshrc ZSH/zshrc
+cp $HOME/.tmux.conf tmux.conf
+
