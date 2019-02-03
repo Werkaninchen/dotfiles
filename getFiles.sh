95bce491749139c1a037eb772f0b548117729782
Werkaninchen <jeroen.trzaska@gmail.com>
2019-02-03 10:38:17 +0100

    adding get and set scripts

---
+++ getFiles.sh
@@ -0,0 +1,5 @@
+!/bin/bash
+cp ~/.config/terminator /terminator
+cp ~/.config/i3 /i3
+cp ~/.vimrc /vimrc
+cp ~/.zshrc /ZSH/zshrc
---
+++ setFiles.sh
@@ -0,0 +1,5 @@
+!/bin/bash
+cp /vimrc ~/.vimrc
+cp /terminator ~/.config/terminator
+cp /i3 ~/.config/i3
+cp /ZSH/zshrc ~/.zshrc
