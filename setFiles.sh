9a5ea5e0ee34791a2b728c269388e0e51dfe19a0
Werkaninchen <jeroen.trzaska@gmail.com>
2019-02-05 00:05:22 +0100

    begun adding second set of configs

--- setFiles.sh
+++ setFiles.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-cp vimrc $HOME/vimrc
-cp terminator $HOME/.config/terminator
+cp vimrc $HOME/.vimrc
+cp -r terminator $HOME/.config/terminator
 cp i3 $HOME/.config/i3
 cp ZSH/zshrc $HOME/.zshrc
 cp tmux.conf $HOME/.tmux.conf
