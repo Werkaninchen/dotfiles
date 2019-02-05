4b42044a124ecba57c79605a3df540f715f1cc5d
Werkaninchen <jeroen.trzaska@gmail.com>
2019-02-05 14:03:29 +0100

    added checks for exsisting files

--- setFiles.sh
+++ setFiles.sh
@@ -1,8 +1,17 @@
+#!/bin/bash
 
-
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+if [! -d "~/.tmux/plugins/tpm"]
+then
+	git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+else
+	cd  ~/.tmux/plugins/tpm 
+	git pull
+fi
+if [! -f "~/.vim/autoload/plug.vim"]
+then
+	curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
 	    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+fi	
 
 cp vimrc $HOME/.vimrc
 cp terminatorConfig $HOME/.config/terminator/config
@@ -12,5 +21,7 @@ cp -rf ZSH/plugins $ZSH/custom/plugins
 cp -rf ZSH/themes $ZSH/cusom/themes
 cp tmux.conf $HOME/.tmux.conf
 cd $ZSH/themes 
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git zsh-syntax-highlighting.theme
-
+if [! -d "zsh-syntax-highlighting.theme"]
+then
+	git clone https://github.com/zsh-users/zsh-syntax-highlighting.git zsh-syntax-highlighting.theme
+fi
