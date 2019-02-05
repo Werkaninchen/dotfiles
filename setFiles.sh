ce6dcf394b74832bfe9b72d6c3da7ea24fd9c5a1
Werkaninchen <jeroen.trzaska@gmail.com>
2019-02-05 13:04:55 +0100

    added dependency checker to set script

--- setFiles.sh
+++ setFiles.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+	    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
 cp vimrc $HOME/.vimrc
 cp terminatorConfig $HOME/.config/terminator/config
 cp -r i3 $HOME/.config/i3
