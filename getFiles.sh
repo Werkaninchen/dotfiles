4716d84b9740fc2b008e0cea909fa028be332c34
Werkaninchen <jeroen.trzaska@gmail.com>
2019-02-05 12:54:52 +0100

    changed get script

--- getFiles.sh
+++ getFiles.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-cp -rf $HOME/.config/terminator/config terminatorConfig
+cp $HOME/.config/terminator/config terminatorConfig
 cp -rf $HOME/.config/i3 i3
 cp $HOME/.vimrc vimrc
 cp $HOME/.zshrc ZSH/zshrc
 cp -rf $ZSH/custom/plugins ZSH/plugins
 cp -rf $ZSH/custom/themes ZSH/themes
-cp -rf $ZSH/themes/zsh-syntax-highlighting.theme ZSH/zsh-syntax-highlighting.theme
 cp $HOME/.tmux.conf tmux.conf
 
